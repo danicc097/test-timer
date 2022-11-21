0c2e26b7a7424e33b921f7bb68df0c5e04d6a8a2
Daniel Castro <danicc097@gmail.com>
2022-11-21 07:42:52 +0000

    notes

--- pgbackuptimer/test-timer-setup.sh
+++ pgbackuptimer/test-timer-setup.sh
@@ -4,3 +4,7 @@ mkdir -p "$HOME"/.config/systemd/user
 cp test-timer.timer "$HOME"/.config/systemd/user/
 cp test-timer.service "$HOME"/.config/systemd/user/
 systemctl enable --now --user test-timer.timer # create symlinks and start timer
+
+# check it exists for the current user:
+systemctl --user list-timers --all # only show timers for the user, remove --user to see everything EXCEPT user ones
+# we probably need to log into the user directly, not through su - <user> (bus error)
