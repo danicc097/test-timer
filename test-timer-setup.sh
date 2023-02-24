50d250214a2422a6ec746ca9db01ebae2e6fed39
danicc097 <71724149+danicc097@users.noreply.github.com>
2023-02-24 10:43:04 +0100

    Update test-timer-setup.sh

--- test-timer-setup.sh
+++ test-timer-setup.sh
@@ -4,3 +4,5 @@ mkdir -p "$HOME"/.config/systemd/user
 cp test-timer.timer "$HOME"/.config/systemd/user/
 cp test-timer.service "$HOME"/.config/systemd/user/
 systemctl enable --now --user test-timer.timer # create symlinks and start timer
+loginctl list-users
+echo "Ensure to `loginctl enable-linger` for the current user"
